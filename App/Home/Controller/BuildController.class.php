ba96a3e01b9a42291729226bc49ff84883a069cb
代码狮 <1173197065@qq.com>
2019-01-07 21:35:16 +0800

    更新首页和招生简章

--- App/Home/Controller/BuildController.class.php
+++ App/Home/Controller/BuildController.class.php
@@ -1,39 +1,44 @@
 <?php
+
 /**
-* +----------------------------------------------------------------------
-* 创建日期：2017年12月12日
-* 最新修改时间：2017年12月12日
-* +----------------------------------------------------------------------
-* https：//github.com/ALNY-AC
-* +----------------------------------------------------------------------
-* 微信：AJS0314
-* +----------------------------------------------------------------------
-* QQ:1173197065
-* +----------------------------------------------------------------------
-* #####楼宇控制器#####
-* @author 代码狮
-*
-*/
+ * +----------------------------------------------------------------------
+ * 创建日期：2017年12月12日
+ * 最新修改时间：2017年12月12日
+ * +----------------------------------------------------------------------
+ * https：//github.com/ALNY-AC
+ * +----------------------------------------------------------------------
+ * 微信：AJS0314
+ * +----------------------------------------------------------------------
+ * QQ:1173197065
+ * +----------------------------------------------------------------------
+ * #####楼宇控制器#####
+ * @author 代码狮
+ *
+ */
 
 namespace Home\Controller;
+
 use Think\Controller;
-class BuildController extends CommonController {
-    public function build() {
+
+class BuildController extends CommonController
+{
+    public function build()
+    {
         
         //取总校的科目
-        $model=M('exam');
-        $where=[];
-        $where['school_id']=3;
-        $exam=$model->where($where)->order('sort desc')->select();
-        $this->assign('exam',$exam);
-        
-        $model=M('teacher');
-        $teacher=$model->order('sort desc')->select();
-        $this->assign('teacher',$teacher);
-        
-        $this -> display();
-        
+        $model = M('exam');
+        $where = [];
+        $where['school_id'] = 3;
+        $exam = $model->where($where)->order('sort desc')->select();
+        $this->assign('exam', $exam);
+
+        $model = M('teacher');
+        $teacher = $model->order('sort desc')->select();
+        $this->assign('teacher', $teacher);
+
+        $this->display();
+
     }
-    
-    
+
+
 }
